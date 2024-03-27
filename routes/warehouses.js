748dc4667f637aabe6354d78965d17de8a3b9e1f
Zoe <66967044+trumerfe@users.noreply.github.com>
2024-03-27 17:22:21 -0400

    Created post request for new warehouse with validation

--- routes/warehouses.js
+++ routes/warehouses.js
@@ -2,7 +2,10 @@ const express = require("express");
 const router = express.Router();
 const { v4: uuidv4 } = require('uuid')
 
+const warehouseController = require('../controllers/warehouses-controllers');
 
 
+router.route('/')
+  .post(warehouseController.postNewWarehouse)
 
 module.exports = router;
