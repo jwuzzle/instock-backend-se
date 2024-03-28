4e21b26461184bc5a0eca52ddcab6ec88338ba5b
Edward Vargas <edthevargas@gmail.com>
2024-03-28 15:22:54 -0400

    /warehouse gets all warehouses

--- routes/warehouses.js
+++ routes/warehouses.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { v4: uuidv4 } = require('uuid')
+const { v4: uuidv4 } = require("uuid");
 
-const warehouseController = require('../controllers/warehouses-controllers');
+const warehouseController = require("../controllers/warehouses-controllers");
 
+router.route("/").post(warehouseController.postNewWarehouse);
 
-router.route('/')
-  .post(warehouseController.postNewWarehouse)
+router.route("/").get(warehouseController.getAllWarehouses);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const app = express();
 const cors = require("cors");
 const path = require("path");
 require("dotenv").config();
-const warehouseRoutes = require('./routes/warehouses');
-const inventoryRoutes = require('./routes/inventory');
+const warehouseRoutes = require("./routes/warehouses");
+const inventoryRoutes = require("./routes/inventory");
 
 //port
 const PORT = process.env.PORT || 8080;
@@ -14,16 +14,14 @@ app.use(cors());
 app.use(express.json());
 
 //basic routes for the APIs
-app.use('/warehouses', warehouseRoutes);
-app.use('/inventory', inventoryRoutes);
-app.use('/', (_req, res)=>{
-    res.status(200).json({
-        'project name':'Instock', 
-        'description': "Group Project",
-        'createdBy': "the best team in whole of SOHO NYC"
-    })
-})
+app.use("/warehouses", warehouseRoutes);
+app.use("/inventory", inventoryRoutes);
+app.use("/", (_req, res) => {
+   res.status(200).json({
+      "project name": "Instock",
+      description: "Group Project",
+      createdBy: "the best team in whole of SOHO NYC",
+   });
+});
 
-app.listen(PORT, () =>
-    console.log(`Server listening on port ${PORT}`)
-);
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
