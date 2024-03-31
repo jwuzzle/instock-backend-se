d6d3a0cff354f8ef37ad1f1a0990a2da9bd17c88
Sanchez Fernandes <sanchezfernandesmusic@gmail.com>
2024-03-30 23:05:15 -0400

    delete warehouse

--- controllers/warehouses-controllers.js
+++ controllers/warehouses-controllers.js
@@ -1,38 +1,59 @@
 const knex = require("knex")(require("../knexfile"));
 
 const postNewWarehouse = async (req, res) => {
-  console.log(req.body);
+    console.log(req.body);
 
-  if (
-    !req.body.warehouse_name ||
-    !req.body.address ||
-    !req.body.city ||
-    !req.body.country ||
-    !req.body.contact_name ||
-    !req.body.contact_position ||
-    !req.body.contact_phone ||
-    !req.body.contact_email
-  ) {
-    return res.status(400).json({
-      message: "Please provide all required values",
-    });
-  }
+    if (
+        !req.body.warehouse_name ||
+        !req.body.address ||
+        !req.body.city ||
+        !req.body.country ||
+        !req.body.contact_name ||
+        !req.body.contact_position ||
+        !req.body.contact_phone ||
+        !req.body.contact_email
+    ) {
+        return res.status(400).json({
+            message: "Please provide all required values",
+        });
+    }
 
-  try {
-    const result = await knex("warehouses").insert(req.body);
-    const newWarehouseId = result[0];
-    const createdWarehouse = await knex("warehouses").where({
-      id: newWarehouseId,
-    });
+    try {
+        const result = await knex("warehouses").insert(req.body);
+        const newWarehouseId = result[0];
+        const createdWarehouse = await knex("warehouses").where({
+            id: newWarehouseId,
+        });
 
-    res.status(201).json(createdWarehouse);
-  } catch (error) {
-    res.status(500).json({
-      message: `Unable to create new warehouse: ${error}`,
-    });
-  }
+        res.status(201).json(createdWarehouse);
+    } catch (error) {
+        res.status(500).json({
+            message: `Unable to create new warehouse: ${error}`,
+        });
+    }
+};
+
+const deleteWarehouse = async (req, res) => {
+    try {
+        const rowsDeleted = await knex("warehouses")
+            .where({ id: req.params.id })
+            .delete();
+
+        if (rowsDeleted === 0) {
+            return res
+                .status(404)
+                .json({ message: `Warehouse with ID ${req.params.id} not found` });
+        }
+
+        res.sendStatus(204);
+    } catch (error) {
+        res.status(500).json({
+            message: `Unable to delete Warehouse: ${error}`
+        });
+    }
 };
 
 module.exports = {
-  postNewWarehouse,
+    postNewWarehouse,
+    deleteWarehouse,
 };
